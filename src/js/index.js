d2795d4a3a0a90daf5ccd73463cf430e9394c656
Sergii Karpus <49605599+Siren1790@users.noreply.github.com>
2023-01-05 16:45:53 +0200

    Work

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,53 @@
 const API_KEY = '32586703-3cda94dac50b012465c4c9243';
 const API_URL = 'https://pixabay.com/api/';
 
+refs = {
+    formRef: document.querySelector('#search-form'),
+    loadBtnRef: document.querySelector('.js-load-more'),
+    gallery: document.querySelector('.gallery'),
+}
+
+class img {
+    constructor({searchValue}){
+        this.searchValue = searchValue;
+        this.currentPage = 1;
+    }
+
+    fetchImg(){
+
+    }
+
+    nextPage(){
+        this.currentPage += 1;
+        this.fetchImg();
+    }
+}
+
+
+function serchImg (event){
+    event.preventDefault();
+    console.log(event.target.searchQuery.value);
+    // *********
+    event.target.searchQuery.value = '';
+}
+
+function markupNext (){
+    // *********
+    console.log("LOAD MORE...")
+}
+
+function renderUserListItems(users) {
+    const markup = users
+    .map((user) => `<li class="item">
+        <p><b>Name</b>: ${user.name}</p>
+        <p><b>Email</b>: ${user.email}</p>
+        <p><b>Company</b>: ${user.company.name}</p>
+        </li>`
+    )
+    .join("");
+    userList.innerHTML = markup;
+}
+
+
+refs.formRef.addEventListener('submit', serchImg);
+refs.loadBtnRef.addEventListener('click', markupNext);
